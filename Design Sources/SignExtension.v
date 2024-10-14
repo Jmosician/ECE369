181435ef6329deceaac15c8abc97f1e4f2166900
Jerald Matthew Casilio Ocaya <jmocaya@arizona.edu>
2024-10-14 11:37:58 -0700

    Create SignExtension.v
    Made Sign extension

---
+++ Design Sources/SignExtension.v
@@ -0,0 +1,21 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// ECE369 - Computer Architecture
+// 
+// Module - SignExtension.v
+// Description - Sign extension module.
+////////////////////////////////////////////////////////////////////////////////
+module SignExtension(in, out);
+
+    /* A 16-Bit input word */
+    input [15:0] in;
+    
+    /* A 32-Bit output word */
+    output [31:0] out;
+    
+    /* Fill in the implementation here ... */
+    assign out = {{16{in[15]}}, in};
+    //repeats MSB(most significant bit) 16 times for two's complement)
+
+endmodule
