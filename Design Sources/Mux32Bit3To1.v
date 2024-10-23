9738bed6eef9685ec33307617e49b86af903d2da
logangeorge03 <logangeorge0311@gmail.com>
2024-10-22 18:46:25 -0700

    muxes

--- Design Sources/Mux32Bit3To1.v
+++ Design Sources/Mux32Bit3To1.v
@@ -3,24 +3,27 @@
 ////////////////////////////////////////////////////////////////////////////////
 // ECE369 - Computer Architecture
 // 
-// Module - Mux5Bit2To1.v
-// Description - Performs signal multiplexing between 2 5-Bit words.
+// Module - Mux32Bit3To1.v
+// Description - Performs signal multiplexing between 3 32-Bit words.
 ////////////////////////////////////////////////////////////////////////////////
 
-module Mux5Bit2To1(out, inA, inB, sel);
+module Mux32Bit2To1(out, inA, inB, inC, sel);
 
-    output reg [4:0] out;
+    output reg [31:0] out;
     
-    input [4:0] inA;
-    input [4:0] inB;
+    input [31:0] inA;
+    input [31:0] inB;
+    input [31:0] inC;
     input sel;
 
     /* Fill in the implementation here ... */ 
-    always @(sel, inA, inB) begin
+    always @(sel, inA, inB, inC) begin
     if (sel == 1) begin
     out <= inA; end
-    else begin
+    else if (sel==0) begin
     out <= inB; end
+    else begin
+    out <= inC; end
     end
 
 endmodule
