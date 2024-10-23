0a1f7e5a90694b272191a5dd9ca7124cb1a6a178
Jerald Matthew Casilio Ocaya <jmocaya@arizona.edu>
2024-10-22 20:00:19 -0700

    Controller done

--- Design Sources/Controller.v
+++ Design Sources/Controller.v
@@ -3,9 +3,9 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date: 10/21/2024 04:16:47 PM
+// Create Date: 10/22/2024 11:17:59 AM
 // Design Name: 
-// Module Name: Control
+// Module Name: Controller
 // Project Name: 
 // Target Devices: 
 // Tool Versions: 
@@ -20,17 +20,19 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module Control(
-    Instruction, PCSrc, RegWrite, jump_target, ALUSrc, ALUOp, RegDst, Branch, MemWrite, MemRead, MemToReg
+module Controller(
+    Instruction, PCSrc, RegWrite, jump_target, ALUSrc, ALUOp, RegDst, Branch, MemWrite, MemRead, MemToReg, jr, ra, store, load, jal
     );
     
     input [31:0] Instruction;
-    output reg PCSrc, RegWrite, jump_target, ALUSrc, RegDst, Branch, MemWrite, MemRead, MemToReg;
+    output reg PCSrc, RegWrite, jump_target, ALUSrc, RegDst, Branch, MemWrite, MemRead, MemToReg, jr, ra, jal;
     output reg [5:0] ALUOp;
+    output reg [1:0] load, store;
     
     always @(Instruction[31:26]) begin
         case(Instruction[31:26]) 
-            6'b000000: begin         //R-Type
+            6'b000000: begin 
+            //R-Type        
             Branch = 0;
             RegWrite = 1;
             jump_target = 0;
@@ -38,6 +40,11 @@ module Control(
             MemWrite = 0;
             MemRead = 0;
             MemToReg = 1; 
+            jr = 0; 
+            ra = 0;
+            store = 0;
+            load = 0;
+            jal = 0;
             
             
             case(Instruction[5:0]) 
@@ -77,9 +84,27 @@ module Control(
             ALUOp = 6'b000010;
             ALUSrc = 1; end
             
+            6'b001000: begin //jr because opcode is zero for it
+            ALUOp = 6'b110000;
+            Branch = 1;
+            RegWrite = 0;
+            jump_target = 0;
+            ALUSrc = 1;
+            RegDst = 1;
+            MemWrite = 0;
+            MemRead = 0;
+            MemToReg = 0; 
+            jr = 1; 
+            ra = 0;
+            store = 0;
+            load = 0;
+            jal = 0;
+            end
+            
             endcase
             end
             
+            //Immediate
             6'b011100: begin  //mul
             ALUOp = 6'b111000;
             Branch = 0;
@@ -90,6 +115,11 @@ module Control(
             MemRead = 0;
             MemToReg = 1; 
             ALUSrc = 0;
+            jr = 0; 
+            ra = 0;
+            store = 0;
+            load = 0;
+            jal = 0;
             end
             
             6'b000100: begin //beq
@@ -102,7 +132,11 @@ module Control(
             MemWrite = 0;
             MemRead = 0;
             MemToReg = 0; 
-            //fix me: if not branch
+            jr = 0; 
+            ra = 0;
+            store = 0;
+            load = 0;
+            jal = 0;
             end
             
             6'b001100: begin    //andi
@@ -115,6 +149,11 @@ module Control(
             MemWrite = 0;
             MemRead = 0;
             MemToReg = 1; 
+            jr = 0; 
+            ra = 0;
+            store = 0;
+            load = 0;
+            jal = 0;
             end
             
             6'b001101: begin    //ori
@@ -127,6 +166,11 @@ module Control(
             MemWrite = 0;
             MemRead = 0;
             MemToReg = 1; 
+            jr = 0; 
+            ra = 0;
+            store = 0;
+            load = 0;
+            jal = 0;
             end
             
             6'b001110: begin    //xori
@@ -139,6 +183,11 @@ module Control(
             MemWrite = 0;
             MemRead = 0;
             MemToReg = 1; 
+            jr = 0; 
+            ra = 0;
+            store = 0;
+            load = 0;
+            jal = 0;
             end
             
             6'b001010: begin    //slti
@@ -151,6 +200,11 @@ module Control(
             MemWrite = 0;
             MemRead = 0;
             MemToReg = 1; 
+            jr = 0; 
+            ra = 0;
+            store = 0;
+            load = 0;
+            jal = 0;
             end
             
             6'b000001: begin    //bgez or bltz
@@ -162,11 +216,16 @@ module Control(
             MemWrite = 0;
             MemRead = 0;
             MemToReg = 1; 
+            jr = 0; 
+            ra = 0;
+            store = 0;
+            load = 0;
+            jal = 0;
             
-            if(Instruction[20:16] == 0) 
-            ALUOp = 001011;  //bltz
-            else if (Instruction[20:16] == 1)
-            ALUOp = 001000;  //bgez
+            if(Instruction[20:16] == 0) begin
+            ALUOp = 001011;  end //bltz
+            else if (Instruction[20:16] == 1) begin
+            ALUOp = 001000;  end //bgez
             end
             
             6'b000101: begin //bne
@@ -179,6 +238,11 @@ module Control(
             MemWrite = 0;
             MemRead = 0;
             MemToReg = 0; 
+            jr = 0; 
+            ra = 0;
+            store = 0;
+            load = 0;
+            jal = 0;
             end
             
             6'b000111: begin //bgtz
@@ -191,6 +255,11 @@ module Control(
             MemWrite = 0;
             MemRead = 0;
             MemToReg = 0; 
+            jr = 0; 
+            ra = 0;
+            store = 0;
+            load = 0;
+            jal = 0;
             end
             
             6'b000110: begin //blez
@@ -203,16 +272,172 @@ module Control(
             MemWrite = 0;
             MemRead = 0;
             MemToReg = 0; 
+            jr = 0; 
+            ra = 0;
+            store = 0;
+            load = 0;
+            jal = 0;
             end
             
+            //jumps
+            6'b000010: begin //j
+            ALUOp = 6'b110000;
+            Branch = 1;
+            RegWrite = 0;
+            jump_target = 1;
+            ALUSrc = 1;
+            RegDst = 1;
+            MemWrite = 0;
+            MemRead = 0;
+            MemToReg = 0; 
+            jr = 0; 
+            ra = 0;
+            store = 0;
+            load = 0;
+            jal = 0;
+            end 
             
+            6'b000010: begin //j
+            ALUOp = 6'b110000;
+            Branch = 1;
+            RegWrite = 0;
+            jump_target = 1;
+            ALUSrc = 1;
+            RegDst = 1;
+            MemWrite = 0;
+            MemRead = 0;
+            MemToReg = 0; 
+            jr = 0; 
+            ra = 0;
+            store = 0;
+            load = 0;
+            jal = 0;
+            end 
             
+            6'b000011: begin //jal
+            ALUOp = 6'b110000;
+            Branch = 1;
+            RegWrite = 0;
+            jump_target = 1;
+            ALUSrc = 1;
+            RegDst = 1;
+            MemWrite = 0;
+            MemRead = 0;
+            MemToReg = 0; 
+            jr = 0; 
+            ra = 1;
+            store = 0;
+            load = 0;
+            jal = 1;
+            end 
             
-          
+            //memory instructions
+            //load
+            6'b100011: begin //lw
+            ALUOp = 6'b100000;
+            Branch = 0;
+            RegWrite = 1;
+            jump_target = 0;
+            ALUSrc = 1;
+            RegDst = 0;
+            MemWrite = 0;
+            MemRead = 1;
+            MemToReg = 0; 
+            jr = 0; 
+            ra = 0;
+            store = 0;
+            load = 0;
+            jal = 0;
+            end 
             
+            6'b100001: begin //lh
+            ALUOp = 6'b100000;
+            Branch = 0;
+            RegWrite = 1;
+            jump_target = 0;
+            ALUSrc = 1;
+            RegDst = 0;
+            MemWrite = 0;
+            MemRead = 1;
+            MemToReg = 0; 
+            jr = 0; 
+            ra = 0;
+            store = 0;
+            load = 1;
+            jal = 0;
+            end 
             
+            6'b100000: begin //lb
+            ALUOp = 6'b100000;
+            Branch = 0;
+            RegWrite = 1;
+            jump_target = 0;
+            ALUSrc = 1;
+            RegDst = 0;
+            MemWrite = 0;
+            MemRead = 1;
+            MemToReg = 0; 
+            jr = 0; 
+            ra = 0;
+            store = 0;
+            load = 2;
+            jal = 0;
+            end 
+            
+            //store
+            6'b101011: begin //sw
+            ALUOp = 6'b100000;
+            Branch = 0;
+            RegWrite = 0;
+            jump_target = 0;
+            ALUSrc = 1;
+            RegDst = 0;
+            MemWrite = 0;
+            MemRead = 1;
+            MemToReg = 0; 
+            jr = 0; 
+            ra = 0;
+            store = 0;
+            load = 0;
+            jal = 0;
+            end 
+            
+            6'b101001: begin //sh
+            ALUOp = 6'b100000;
+            Branch = 0;
+            RegWrite = 0;
+            jump_target = 0;
+            ALUSrc = 1;
+            RegDst = 0;
+            MemWrite = 0;
+            MemRead = 1;
+            MemToReg = 0; 
+            jr = 0; 
+            ra = 0;
+            store = 1;
+            load = 0;
+            jal = 0;
+            end 
+            
+            6'b101000: begin //sb
+            ALUOp = 6'b100000;
+            Branch = 0;
+            RegWrite = 0;
+            jump_target = 0;
+            ALUSrc = 1;
+            RegDst = 0;
+            MemWrite = 0;
+            MemRead = 1;
+            MemToReg = 0; 
+            jr = 0; 
+            ra = 0;
+            store = 2;
+            load = 0;
+            jal = 0;
+            end 
+        
            
-    
      endcase
    end
 endmodule
+
