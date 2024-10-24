7caaaa291905bdc6b8e6c1c870671e31033b3a9b
logangeorge03 <logangeorge0311@gmail.com>
2024-10-23 22:27:56 -0700

    idk

--- Design_Sources/ALU32Bit.v
+++ Design_Sources/ALU32Bit.v
@@ -39,40 +39,40 @@ module ALU32Bit(ALUControl, A, B, ALUResult, Zero);
     always @(*) begin //do everything except data & jump
         case(ALUControl)
         6'b000000: begin //sll
-            ALUResult <= A << B[10:6]; end
+            ALUResult = A << B[10:6]; end
         6'b000010: begin //srl
-            ALUResult <= A >> B[10:6]; end
+            ALUResult = A >> B[10:6]; end
         6'b100000: begin //add & addi
-            ALUResult <= A + B; end
+            ALUResult = A + B; end
         6'b100010: begin //sub, beq
-            ALUResult <= A - B; end
+            ALUResult = A - B; end
         6'b111000: begin //mul
-            ALUResult <= A * B; end
+            ALUResult = A * B; end
         6'b100100: begin  //and, andi
-            ALUResult <= A & B; end
+            ALUResult = A & B; end
         6'b100101: begin //or, ori
-            ALUResult <= A | B; end
+            ALUResult = A | B; end
         6'b100111: begin //nor
-            ALUResult <= ~(A | B); end
+            ALUResult = ~(A | B); end
         6'b100110: begin //xor
-            ALUResult <= A ^ B; end
+            ALUResult = A ^ B; end
         6'b101010: begin //slt, slti
-            ALUResult <= (A < B); end
+            ALUResult = (A < B); end
         6'b001000: begin //bgez
-            ALUResult <= A < 0; end
+            ALUResult = A < 0; end
         6'b001001: begin  //bne
-            ALUResult <= (A == B); end
+            ALUResult = (A == B); end
         6'b001010: begin //blez
-            ALUResult <= A > 0; end
+            ALUResult = A > 0; end
         6'b001011: begin //bltz
-            ALUResult <= A >= 0; end
+            ALUResult = A >= 0; end
         6'b001111: begin //bgtz
-            ALUResult <= A <= 0; end
+            ALUResult = A <= 0; end
         6'b110000: begin //j
-            ALUResult <= 0; end
+            ALUResult = 0; end
         
         default: begin
-            ALUResult <= 32'bX; end
+            ALUResult = 32'bX; end
         
         endcase
         
@@ -87,8 +87,8 @@ module ALU32Bit(ALUControl, A, B, ALUResult, Zero);
   
         
     
+
        
         
-    
 endmodule
 
---
+++ Simulation_Sources_TestBench/Controller_tb.v
@@ -0,0 +1,55 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/23/2024 09:42:54 PM
+// Design Name: 
+// Module Name: Controller_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Controller_tb();
+    reg [31:0] Instruction;
+    wire RegWrite, jump_target, ALUSrc, RegDst, Branch, MemWrite, MemRead, MemToReg, jr, ra, jal;
+    wire [5:0] ALUOp;
+    wire [1:0] load, store;
+    
+    
+    Controller u0(
+         .Instruction(Instruction),
+         .RegWrite(RegWrite),
+         .jump_target(jump_target),
+         .ALUSrc(ALUSrc),
+         .ALUOp(ALUOp),
+         .RegDst(RegDst),
+         .Branch(Branch), 
+         .MemWrite(MemWrite), 
+         .MemRead(MemRead),
+         .MemToReg(MemToReg),
+         .jr(jr), 
+         .ra(ra), 
+         .store(store), 
+         .load(load), 
+         .jal(jal)
+    );
+    
+    initial begin
+        Instruction=32;
+        #10
+        Instruction=536870944;
+    end
+
+
+endmodule
